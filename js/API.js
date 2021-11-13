811eaca62a6f5ab435b2368a1ed8f77d07c73e64
Laima Petrauskiene <laima@tinklai.net>
2021-11-13 21:20:34 +0200

    5: papildytas API failas

---
+++ js/API.js
@@ -0,0 +1,29 @@
+const baseURL = "http://localhost:3000";
+
+
+class API {
+    static fetchToys = (success, failure) => {
+        fetch(`${baseURL}/toys`)
+            .then(res => res.json())
+            .then(success)
+            .catch(failure)
+    }
+
+
+    static deleteToy = (id, success, failure) => {
+        fetch(`${baseURL}/toys/${id}`, { method: 'DELETE' })
+            .then(res => res.status === 200 ? success() : failure(res.statusText))
+            .catch(failure)
+    }
+}
+
+// API.fetchToys(
+//    console.log,
+//    console.error
+// )
+
+API.deleteToy(
+    "4",
+    () => console.log('Ištrinta sėkmingai'),
+    console.error
+)
